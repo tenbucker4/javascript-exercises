9e0bcccdd2b38555e96dc46b0db48d6522ca37a0
Ben Tucker <bwtucker4@gmail.com>
2022-04-08 17:17:35 -0700

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+function ftoc(tempF) {
+  let number;
+  number = ((tempF - 32) * (5/9));
+  let numberRound = parseFloat(number.toFixed(1));
+  return numberRound;
 };
 
-const ctof = function() {
-
+function ctof(tempC) {
+  let number;
+  number = ((tempC * (9/5)) + 32);
+  let numberRound = parseFloat(number.toFixed(1));
+  return numberRound;
 };
 
 // Do not edit below this line

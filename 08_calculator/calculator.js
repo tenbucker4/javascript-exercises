4edc2513f8041b18a820eec4353426fc196bad2a
Ben Tucker <bwtucker4@gmail.com>
2022-04-18 12:05:37 -0700

    Complete JS exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	if (input === 3) return 1;
+  let result = 1;
+  for (let i = input; i > 0; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line

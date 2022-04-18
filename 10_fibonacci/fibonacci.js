b537fc7189737363527bd9b869e9c535e3592653
Ben Tucker <bwtucker4@gmail.com>
2022-04-18 12:20:54 -0700

    Complete JS exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < index; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
